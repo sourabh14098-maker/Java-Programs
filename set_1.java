6c42274cf98687b4ddaceffed02f7ae5e4432679
Sourabh Raj <sourabh14098@gmail.com>
2026-06-24 18:51:58 +0530

    code updated

--- set_1.java
+++ set_1.java
@@ -1,87 +1,270 @@
-// 3 marks question hai set 1 code only 
+// 3 marks question hai set 1 code only
 
-//  Write a Java Program Using Interface.
+// Write a Java Program Using Interface.
 
 // interface Demo {
-//     void show();
+// void show();
 // }
 
 // class Test implements Demo {
 
-//     public void show() {
-//         System.out.println("Interface Example");
-//     }
-
-//     public static void main(String[] args) {
-//         Test t = new Test();
-//         t.show();
-//     }
+// public void show() {
+// System.out.println("Interface Example");
 // }
 
-//  Java Program Demonstrating Multilevel Inheritance
+// public static void main(String[] args) {
+// Test t = new Test();
+// t.show();
+// }
+// }
+
+// Java Program Demonstrating Multilevel Inheritance
 
 // class Vehicle {
-//     void show() {
-//         System.out.println("this is a Vechicle");
-//     }
+// void show() {
+// System.out.println("this is a Vechicle");
+// }
 // }
 
 // class car extends Vehicle {
-//     void start() {
-//         System.out.println("car is starting");
-//     }
+// void start() {
+// System.out.println("car is starting");
+// }
 
 // }
 
 // class Cngcar extends car {
-//     void gas() {
-//         System.out.println("Cng car is charging");
-//     }
+// void gas() {
+// System.out.println("Cng car is charging");
+// }
 // }
 
 // class Main {
-//     public static void main(String[] args) {
+// public static void main(String[] args) {
 
-//         Cngcar c = new Cngcar();
+// Cngcar c = new Cngcar();
 
-//         c.show();
+// c.show();
 
-//     }
+// }
 // }
 
 // Q8. Write a Java Program Using throws Keyword. (3 Marks)
 
 // class Demo {
 
-//     static void test() throws ArithmeticException {
-//         throw new ArithmeticException("Error");
-//     }
+// static void test() throws ArithmeticException {
+// throw new ArithmeticException("Error");
+// }
 
-//     public static void main(String[] args) {
-//         test();
-//     }
+// public static void main(String[] args) {
+// test();
+// }
 // }
 
 // Q10. Write a Java Program Using HashSet. (3 Marks)
 
-// HashSet is a class in Java that stores unique elements (no duplicates allowed). 
-// It is part of the Java Collections Framework and uses a hash table internally for fast storage and retrieval.
+// HashSet is a class in Java that stores unique elements (no duplicates
+// allowed).
+// It is part of the Java Collections Framework and uses a hash table internally
+// for fast storage and retrieval.
 
 // import java.util.HashSet;
 
 // class main {
-//     public static void main(String[] args) {
+// public static void main(String[] args) {
 
-//         HashSet<String> hs = new HashSet<>();
+// HashSet<String> hs = new HashSet<>();
 
-//         hs.add("java");
-//         hs.add("python");
+// hs.add("java");
+// hs.add("python");
 
-//         System.out.println(hs);
-//     }
+// System.out.println(hs);
+// }
 // }
 
+// difference betwween paratmeterized and default
 
-// LONG ANSWER CODE STARTS FROM HERE (14 MARKS)
+// class student {
 
- 
+// student() {
+// System.out.println("default constructor");
+// }
+// student(String name) {
+// System.out.println("paramterized constructor");
+// }
+// public static void main(Stirng[] args){
+
+// student s1 = new student();
+// student s2 = new student(rahul);
+// }
+// }
+
+// class divide {
+// public static void main(String[] args){
+
+// try {
+// int a = 10/0;
+
+// }
+
+// catch(ArithmeticException e){
+
+// System.out.println("division by zero not allowed");
+
+// }
+
+// finally {
+
+// System.out.println("program continous");
+// }
+// }
+// }
+
+// Abstraction hides implementation details and shows only essential
+// information.
+
+// It is achieved using abstract class or interface.
+
+// Program
+
+// class abstract shape {
+// abstract void draw();
+
+// }
+// class circle extends shape {
+// void draw(){
+
+// System.out.println("draw a circle");
+// }
+// }
+// class Main {
+// public static void main(String[] args){
+
+// circle c = new circle();
+
+// c.draw();
+// }
+// }
+
+// polymorphism
+// Polymorphism means one method with many forms..
+
+// it has two types :-
+
+// 1.method overloading
+
+// class Mathoperation {
+
+// void add (int a, int b){
+
+// System.out.println(a+b);
+// }
+// void add (int a, int b, int c){
+// System.out.println(a+b+c);
+
+// }
+// public static void main(String[] args){
+
+// Mathoperation m = new Mathoperation();
+
+// m.add(19,38);
+// m.add(12,67,89);
+// }
+// }
+
+// 2.method overrriding
+
+// class Animal {
+// void sound(){
+// System.out.println("animal makes sound");
+// }
+// }
+// class dog extends Animal {
+
+// void sound(){
+// System.out.println("dog barks");
+// }
+// }
+// class cat extends Animal {
+
+// void sound(){
+// System.out.println("cat meows");
+// }
+// }
+// public class Animal {
+// public static void main(String[] args){
+
+// Animal a = new Animal();
+// a.sound();
+
+// dog d = new dog();
+// d.sound();
+
+// cat c = new cat();
+// c.sound();
+
+// }
+// }
+
+// Inheritance
+// Definition
+
+// Inheritance is the process by which one class acquires the properties and
+// methods of another class using the extends keyword.
+
+// class Person {
+
+// void walk(){
+// System.out.println("person walking");
+// }
+// }
+
+// class suresh extends Person {
+
+// void eat(){
+// System.out.println("suresh mango eating");
+// }
+
+// }
+
+// class Main {
+
+// public static void main(String[] args){
+
+// suresh s = new suresh();
+
+// s.eat();
+// s.walk();
+// }
+// }
+
+// Encapsulation
+// Definition
+
+// Encapsulation is the process of wrapping data and methods into a single unit
+// (class) and protecting data using private variables.
+
+// class Student {
+
+// private int age;
+
+// public void setAge(int age){
+// this.age = age;
+// }
+
+// public int getAge() {
+// return age;
+
+// }
+
+// public static void main(String[] args){
+
+// Student s = new Student();
+
+// s.setAge(19);
+
+// System.out.println("Age : "+ s.getAge());
+
+// }
+// }
---
+++ set_2.java
@@ -0,0 +1,319 @@
+// MODULE – 3: Advanced Object-Oriented Concepts
+
+// 1. Constructors in Java
+// Definition
+
+// A constructor is a special method that is automatically called when an object
+// is created. It is used to initialize the object's data members.
+
+// Characteristics
+// Constructor name must be the same as the class name.
+// It has no return type.
+// It is called automatically when an object is created.
+
+// types of constructor :-
+// 1.default
+// 2.parameterized
+// 3.copy
+// 4.private
+
+// 1.Default
+// Takes no parameters.
+// Initializes objects with default or user-defined values.
+
+// class Student {
+
+// Student(){
+// System.out.println("default constructor");
+// }
+// }public class Main {
+// public static void main(String[] args){
+
+// Student s = new Student();
+
+// }
+// }
+
+// 2.parameterized
+// Takes parameters (arguments).
+// Used to initialize objects with different values.
+
+// class Student {
+
+// String name;
+// int age;
+
+// Student(String n, int a) {
+
+// name = n;
+// age = a;
+// }
+// void display (){
+// System.out.println("Name : "+ name);
+// System.out.println("Age : "+ age);
+// }
+// }
+// public class Main {
+// public static void main(String[] args){
+
+// Student s1 = new Student("sourabh",39);
+// s1.display();
+// }
+// }
+
+// 3.Copy Constructor (User-Defined)
+
+// Java does not have a built-in copy constructor, but we can create one
+// ourselves by passing an object of the same class.
+
+// class Student {
+// String name;
+// int age;
+
+// Student(String n, int a){
+// name = n;
+// age = a;
+// }
+// Student(Student s) {
+// name = s.name;
+// age = s.age;
+// }
+// void display() {
+// System.out.println(name + " "+ age);
+// }
+// }
+// public class Main {
+// public static void main(String[] args){
+
+// Student s1 = new Student("rahul",39);
+// Student s2 = new Student(s1);
+
+// s2.display();
+
+// }
+// }
+
+// Method Overloading
+
+// Definition
+
+// Method Overloading means having multiple methods with the same name but
+// different parameters in the same class.
+
+// class Mathoperation {
+
+// void add(int a, int b){
+// System.out.println(a+b);
+// }
+
+// void add(int a, int b, int c){
+// System.out.println(a+b+c);
+
+// }
+
+// public static void main(String[] args){
+
+// Mathoperation m = new Mathoperation();
+
+// m.add(20,10);
+// m.add(4,5,3);
+// }
+// }
+
+// Method Overriding
+
+// Definition
+// Method Overriding occurs when a child class provides its own implementation
+// of a method already defined in the parent class.
+
+// class Animal {
+
+// void sound() {
+// System.out.println("Animal Sound");
+// }
+// }
+
+// class Dog extends Animal {
+
+// void sound() {
+// System.out.println("Dog Barks");
+// }
+// }
+
+// class Main {
+
+// public static void main(String[] args) {
+
+// Dog d = new Dog();
+
+// d.sound();
+// }
+// }
+
+// Abstract Class
+
+// Definition
+// An abstract class is declared using the abstract keyword.
+
+// It cannot be instantiated and may contain abstract and normal methods.
+
+// abstract class Shape {
+
+// abstract void draw();
+// }
+
+// class Circle extends Shape {
+
+// void draw() {
+
+// System.out.println("Drawing Circle");
+// }
+// }
+
+// class Main {
+
+// public static void main(String[] args) {
+
+// Circle c = new Circle();
+
+// c.draw();
+// }
+// }
+
+// Interface
+
+// Definition
+// An Interface is a blueprint of a class that contains abstract methods.
+
+// A class implements an interface using the implements keyword.
+
+// interface Bank {
+
+// void deposite();
+// void withdraw();
+// }
+// class SBI implements Bank {
+
+// public void deposit() {
+// System.out.println("sbi deposit");
+// }
+
+// public void withdraw() {
+// System.out.println("sbi withdraw");
+
+// }
+// }
+// class AXIS implements Bank {
+// public void deposit(){
+// System.out.println("axis deposit");
+
+// }
+// public void withdraw(){
+// System.out.println("axis withdraw");
+
+// }
+// }
+// class Main {
+// public static void main(String[] args){
+
+// SBI s = new SBI();
+
+// s.deposit();
+// s.withdraw();
+// }
+// }
+
+// MODULE – 4: Exception Handling and File I/O (Detailed Notes)
+
+// try Block
+
+// Definition
+// The try block contains code that may generate an exception.
+
+// class Main {
+// public static void main(String[] args){
+// try {
+// int a = 10/0;
+
+// System.out.println(a);
+// }
+// }
+// }
+
+// catch Block
+
+// Definition
+// The catch block handles the exception generated in the try block.
+
+// class Main {
+
+// public static void main(String[] args){
+
+// try {
+// int a = 10 / 0;
+
+// }
+
+// catch(ArithmeticException e) {
+// System.out.println("division by zero is not allowed");
+// }
+// }
+// }
+
+// 4. try-catch Program
+
+// class Main {
+//     public static void main(String[] args){
+
+//         int a = 10;
+//         int b = 0;
+
+//         try {
+//             int c = a / b;
+
+//             System.out.println(c);
+
+//         }
+
+//         catch(ArithmeticException e){
+//             System.out.println("cannot divide by zero");
+
+//         }
+
+//     }
+// }
+
+// finally Block
+
+// Definition
+// The finally block always executes whether an exception occurs or not.
+
+// class Main {
+//     public static void main(String[] args){
+
+//         try {
+//             int a = 10 / 0;
+
+//         }
+//         catch(ArithmeticException e) {
+//             System.out.println("divison by zero is not allowed");
+
+//         }
+//         finally {
+//             System.out.println("operation completed");
+
+//         }
+//     }
+// }
+
+// hrow Keyword
+
+// Definition
+// The throw keyword is used to explicitly create and throw an exception.
+
+class Main {
+    public static void main(String[] args) {
+
+        throw new ArithmeticException("custom exception");
+    }
+}
